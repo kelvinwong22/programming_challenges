ca48947a1289b7c3101a4792f0e08fe810e5e569
kelvinwong22 <wong.k.kelvin@gmail.com>
2016-09-25 18:10:15 -0700

    1.6.3
    Completed challenge in C++

---
+++ 1.6.3/cpp/main.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+#include <iomanip>
+#include <math.h>
+
+unsigned int TERMINATE = 0;
+double SCALE = 0.01;
+
+int main() {
+    unsigned int numberOfStudents;
+
+    std::cin >> numberOfStudents;
+
+    while (numberOfStudents != TERMINATE) {
+        double expenses[numberOfStudents];
+        double total = 0.00;
+        double payback = 0.00;
+        for (unsigned int i = 0; i < numberOfStudents; i++)
+        {
+            std::cin >> expenses[i];
+            total += expenses[i];
+        }
+
+        double average = total / numberOfStudents;
+
+        for (unsigned int i = 0; i < numberOfStudents; i++)
+        {
+            double difference = expenses[i] - average;
+            if (difference > 0)
+            {
+                payback = floorf((difference + payback) * 100 ) / 100;
+            }
+        }
+
+        std::cout << std::setprecision(4) << std::showpoint;
+        std::cout << "The total is: $" << total << std::endl;
+        std::cout << "The minimum that needs to change hands is: $" << payback <<std::endl;
+
+        std::cin >> numberOfStudents;
+    }
+
+    return 0;
+}
